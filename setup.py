3604a9e0ce463683e39f309ab4765b8717626534
dspinosa <dspinosa@innovateteam.com>
2019-07-11 12:18:40 -0700

    added social auth

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     description="Provide oauth2 agol login functionality to django projects",
     url="https://github.com/Innovate-Inc/django-agol-oauth2",
     packages=['agol_oauth2'],
-    install_requires=['django-localflavor'],
+    install_requires=['django-localflavor', 'social-auth-app-django'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
