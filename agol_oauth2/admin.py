e26593dca8178de46ce651a109d020200e5c2b64
Travis Bock <tbock@innovateteam.com>
2020-04-15 07:45:19 -0700

    remove dup unregister

--- agol_oauth2/admin.py
+++ agol_oauth2/admin.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 from .models import AGOLUserFields
 from django.contrib.auth.admin import UserAdmin
 
-admin.site.unregister(User)
-
 
 class AGOLUserFieldsInline(admin.StackedInline):
     model = AGOLUserFields
